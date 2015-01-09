89b2d613418dfa8f4ff4f49d0dfcc2205219e2ea
Rick Johnson <rickarti@sbcglobal.net>
2015-01-09 14:26:43 -0800

    Add Comments for Image Factory

--- tabApp/TAB/TAB/TABImageFactory.m
+++ tabApp/TAB/TAB/TABImageFactory.m
@@ -12,13 +12,16 @@
 
 +(UIImage *) createCancelImage {
 
+    // Setup the Graphics Context and Ref
     UIGraphicsBeginImageContextWithOptions(CGSizeMake(50, 50), NO, 0);
     CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // Draw and Fill a gray circle
     CGContextAddEllipseInRect(context, CGRectMake(10, 10, 30, 30));
     CGContextSetFillColorWithColor(context, [UIColor lightGrayColor].CGColor);
     CGContextFillPath(context);
-    // TODO: Draw X in white
     
+    // Draw a white X centered in the circle
     CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
     CGContextSetLineWidth(context, 2.0f);
     CGContextMoveToPoint(context, 18, 18);
@@ -28,6 +31,7 @@
     CGContextAddLineToPoint(context, 32, 18);
     CGContextStrokePath(context);
     
+    // Collect the reference to return the UIImage and end the Context
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return image;
